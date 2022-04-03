25a1f96b31d12f78baa7b244ab2b41593704bab1
Jayadev <jayadevplanchuvattil@gmail.com>
2022-04-04 01:37:00 +0530

    Collapsing navbar

--- src/topbar/TopBar.js
+++ src/topbar/TopBar.js
@@ -1,22 +1,61 @@
-import "./TopBar.css"
-import logo from "../logo/ecco_logo.png"
+import { useState } from "react";
+import "./TopBar.css";
+import logo from "../logo/ecco_logo.png";
 
 function TopBar() {
-    return (
-        <div className="top">
-            <div className="topLeft">
-                <img className="TopLogo" src={logo} alt="Ecco"></img>
-            </div>
-            <div className="topRight">
-                <ul className="topList">
-                    <li className="topListItem">Home</li>
-                    <li className="topListItem">About Us</li>
-                    <li className="topListItem">Products</li>
-                    <li className="topListItem">Contact Us</li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <img className="TopLogo" src={logo} alt="Ecco"></img>
+      </div>
+      <div className="topRight">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
 
-export default TopBar
+//Once expanded, navbar is not collapsing again
+
+const NavBar = (props) => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded={!isNavCollapsed ? true : false}
+        aria-label="Toggle navigation"
+        onClick={handleNavCollapse}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+        id="navbarNavAltMarkup"
+      >
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">
+            Home
+          </a>
+          <a className="nav-item nav-link" href="#">
+            About Us
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Products
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default TopBar;
